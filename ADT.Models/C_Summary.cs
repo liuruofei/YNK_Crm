b0d0f2124e81c159976af5c02280652de9480f53
liuruofei <604008946@qq.com>
2023-04-11 10:43:15 +0800

    model信息

---
+++ ADT.Models/C_Summary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADT.Models
+{
+   public class C_Summary
+    {
+        public int Id { get; set; }
+
+        public int StudentUid { get; set; }
+        public string openIds { get; set; }
+        public string UserNames { get; set; }
+        public DateTime? CommunicationTime { get; set; }
+        public string SummaryTitle { get; set; }
+        public string Summary { get; set; }
+        public int IsSend { get; set; }
+        public DateTime? SendTime { get; set; }
+        public string CreateUid { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
---
+++ ADT.Models/C_TeacherTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADT.Models
+{
+   public class C_TeacherTask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string SysUid { get; set; }
+        /// <summary>
+        /// 任务状态
+        /// </summary>
+        public int TaskStatus { get; set; }
+        /// <summary>
+        /// 任务内容介绍
+        /// </summary>
+        public string TaskComment { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+
+        public string TaskRemarks { get; set; }
+        public DateTime? CreatTime { get; set; }
+        public string CreateUid { get; set; }
+    }
+}
